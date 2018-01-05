6c04234f744effbcaf15c5f9a8a41a2aff4a3355
freseco <freseco@gmail.com>
2018-01-05 00:36:40 -0600

    Fixed errors

--- SmartRemover.py
+++ SmartRemover.py
@@ -1,10 +1,11 @@
 """
     Author: Francisco Reseco
     Date: 1/1/2018
-    Description: Script to remove the repetitive text in a list of files in a current directorio. It does have license.
+    Description: Script to remove the repetitive text in a list of files in a current directorio.
 """
 import os
 import string
+import sys
 from os import listdir
 from os.path import isfile, join
 
@@ -12,7 +13,7 @@ from os.path import isfile, join
 condition=""
 ListText=[]
 while True:
-    condition=input("Enter text to remove:")
+    condition=input("Enter text to remove(end word to start the removing process):")
     if condition=="end":
         break
     ListText.append(condition)
@@ -23,15 +24,21 @@ mypath=os.getcwd()
 #file list in the directory
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-#print(onlyfiles)
 
 #rename all files removing the text in the Listtext, skiping the python file
 for filename in onlyfiles:
+    
     if ".py" in filename:
         continue
+    #save the original file name
+    newname=filename
+    
+    #remove the strings in the file name
     for textremove in ListText:
-        os.rename(filename,filename.replace(textremove,""))
+        newname = newname.replace(textremove,"")
+        
+    #rename the file    
+    os.rename(filename,newname)
 
 #Exit of the program
 #input("Press enter to exit")
-	
